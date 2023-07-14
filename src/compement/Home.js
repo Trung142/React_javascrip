5685d2615310c26fe8a8509fd3cbd172091e130a
your name <nguonsong47@gmail.com>
2023-07-14 11:05:01 +0700

    React

--- src/compement/Home.js
+++ src/compement/Home.js
@@ -1,26 +1,76 @@
+import Carousel from 'react-bootstrap/Carousel';
+import Pagination from 'react-bootstrap/Pagination';
+import img from '../assets/images/Hinh 3.jpg';
+import img1 from '../assets/images/Hinh 2.jpg';
+import img2 from '../assets/images/Hinh 1.jpg';
+import img3 from '../assets/images/Hinh 5.jpg';
 const Home = () => {
+
+    let active = document.querySelector("active");
+    let items = [];
+    for (let number = 1; number <= 3; number++) {
+        items.push(
+            <Pagination.Item key={number} active={number === active}>
+                {number}
+            </Pagination.Item>,
+        );
+    }
     return (
         <>
-            <div id="body">
-                <div className="box-left">
-                    <h2>
-                        <span>THỨC ĂN</span>
-                        <br />
-                        <span>THƯỢNG HẠNG</span>
-                    </h2>
-                    <p>Chuyền cung cấp các món ăn ngon đảm bảo dinh dưỡng,
-                        đặc biệt là tự nhiên không chất bảo quản, hợp vệ sinh đến người tiều dùng,
-                        phục vụ người dùng một cách hoãn hóa nhất</p>
-                    <button>Mua Ngày</button>
-                </div>
-                <div className="box-right">
-                    <img src="./image/thuc-pham-la-gi-va-cach-phan-loai.jpg" alt="" />
-                    <img src="./image/nhung-meo-nho-giup-chup-anh-san-pham-mon-an-tro-nen-hieu-qua-hon-148-0.jpg" alt="" />
-                    <img src="./image/chup-anh-thuc-an-1.jpg" alt="" />
-                </div>
-                <div id="to_buttom">
+            <Carousel className='img'>
+                <Carousel.Item >
+                    <img
+                        className="d-block w-100"
+                        src={img}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption className='text-body'>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item >
+                    <img
+                        className="d-block w-100 rounded-3 "
+                        src={img1}
+                        alt="Second slide"
+                    />
 
-                </div>
+                    <Carousel.Caption className='text-body'>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item >
+                    <img
+                        className="d-block w-100 rounded-3"
+                        src={img2}
+                        alt="Third slide"
+                    />
+                    <Carousel.Caption className='text-body'>
+                        <h3>Third slide label</h3>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item >
+                    <img
+                        className="d-block w-100 rounded-3"
+                        src={img3}
+                        alt="Forsd slide"
+                    />
+
+                    <Carousel.Caption className='text-body'>
+                        <h3>Third slide label</h3>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            <div>
+                <Pagination>{items}</Pagination>
             </div>
         </>
     )
